ce95a97309af13efbc35411ff18dc84e0b3c3276
shkuropiy <shkuropiy@gmail.com>
2014-08-03 21:04:02 +0800

    Initial commit

---
+++ Microsoft.CodeAnalysis.Delphi.Tests/DelphiSyntaxTreeTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.CodeAnalysis.Delphi.Tests
+{
+    public class DelphiSyntaxTreeTests
+    {
+
+    }
+}
---
+++ Microsoft.CodeAnalysis.Delphi/DelphiSynaxTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.CodeAnalysis.Delphi
+{
+    public abstract class DelphiSynaxTree : SyntaxTree
+    {
+
+    }
+}
